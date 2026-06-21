306b437bde0a531d3b40998ccb63a939851b42e5
Rotno Chandra Das <rotno050@gmail.com>
2026-06-21 15:57:46 +0600

    Done

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const verifyToken = async(req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        //await client.connect();
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
         const database = client.db("ideavault");
